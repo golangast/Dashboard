75a2c1be2c6a6bd67bf2c7ddb566a359637127d3
golangast <zendrulat@gmail.com>
2019-10-22 01:47:11 -0500

    nested structs

--- main.go
+++ main.go
@@ -3,26 +3,31 @@ package main
 import (
 	"fmt"
 	"log"
-	. "github.com/golangast/Dashboard/Page"
-	. "github.com/golangast/Dashboard/User"
+	Pages "github.com/golangast/Dashboard/Page"
+	Users "github.com/golangast/Dashboard/User"
+	Dashboards "github.com/golangast/Dashboard/Dashboards"
 )
 func main() {
 
 	fmt.Println("....starting")
-	Dashboard()
+	
 
-	P, err := CreatePage()
+	P, err := Pages.CreatePage()
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(P)
 
-	U, err := CreateUser()
+	U, err := Users.CreateUser()
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(U)
+
+	D, err := Dashboards.CreateDashboards()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(D)
 }
-func Dashboard() {
-	fmt.Println("this is dashboard")
-}
+
